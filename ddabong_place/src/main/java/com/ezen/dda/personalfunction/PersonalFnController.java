ebe4d1033d7a972609aaa76a42657eba092bb10d
JbPark <wjdqls1199@gamil.com>
2023-10-18 17:52:11 +0900

    콘트롤러 리뷰 구현중

--- ddabong_place/src/main/java/com/ezen/dda/personalfunction/PersonalFnController.java
+++ ddabong_place/src/main/java/com/ezen/dda/personalfunction/PersonalFnController.java
@@ -1,24 +1,28 @@
 package com.ezen.dda.personalfunction;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.io.File;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @Controller
 public class PersonalFnController {
 	@Autowired
 	SqlSession sqlSession;
-
+	
+	//자기 이미지 저장경로로 바꿔주기.
+	static String imagepath = "/image";
 	// 매장 상세 정보,해당 매장 리뷰 등을 출력.
 	@RequestMapping(value = "/detailview")
 	public String detailview(HttpServletRequest request, Model mo) {
@@ -69,7 +73,8 @@ public class PersonalFnController {
 		ArrayList<ReviewDTO> reviewlist = ss.review(storeID);
 		ArrayList<DDAbongDTO> ddabonglist = ss.ddabong(storeID, month);
 		ArrayList<RegistrationDTO> registrationlist = ss.registration(storeID);
-
+		
+		// registrationlist의 다중 이미지 출력
 		// 이미지 파일명을 처리하여 imageList에 저장
 		for (RegistrationDTO registrationDTO : registrationlist) {
 			String image = registrationDTO.getImage(); // dto의 이미지 데이터를 image에 저장.
@@ -82,6 +87,22 @@ public class PersonalFnController {
 				// 저장.
 				List<String> imageList = new ArrayList<>(Arrays.asList(imageFileNames));
 				registrationDTO.setImageList(imageList);
+			}
+		}
+		
+		// reviewlist의 다중 이미지 출력
+		// 이미지 파일명을 처리하여 imageList에 저장
+		for (ReviewDTO reviewDTO : reviewlist) {
+			String image = reviewDTO.getImage(); // dto의 이미지 데이터를 image에 저장.
+
+			// image가 null이거나 비어있을경우가 아닐때 imageFileNames에 콤마와 공백을 기준으로 배열저장.
+			if (image != null && !image.isEmpty()) {
+				String[] imageFileNames = image.split("[,\\s]+");
+
+				// imageFileNames를 리스트화 시키고 ArrayList에 집어넣은 imageList를 만들고 DTO의 imagelist 배열에
+				// 저장.
+				List<String> imageList = new ArrayList<>(Arrays.asList(imageFileNames));
+				reviewDTO.setImageList(imageList);
 			}
 		}
 
@@ -113,6 +134,36 @@ public class PersonalFnController {
 		
 		PersonalFnService ss = sqlSession.getMapper(PersonalFnService.class);
 		ss.reservation(storeID,customer_id,reservation_date,reservation_time,person_num);
+		
+		return "redirect:/main";
+	}
+	
+	@RequestMapping(value = "/review")
+	public String reviewsave(MultipartHttpServletRequest mul) {
+		List<MultipartFile> filelist=mul.getFiles("reviewfile");
+		String content=mul.getParameter("reviewcontent");
+		int star=Integer.parseInt(mul.getParameter("star"));
+		
+		String imagesname="";
+		for(MultipartFile mf : filelist)
+		{
+			String imagefile=mf.getOriginalFilename();
+			imagesname += imagefile + " ";
+			try {
+				mf.transferTo(new File(imagepath+"//"+imagefile));
+			} catch (IllegalStateException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+
+		LocalDate today = LocalDate.now();
+		String dateString = today.toString();
+		
+		PersonalFnService ss=sqlSession.getMapper(PersonalFnService.class);
+		//ss.reviewsave();
+		
 		
 		return "redirect:/main";
 	}
