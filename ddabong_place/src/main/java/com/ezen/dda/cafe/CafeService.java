29c19e579cc3b3dd0ef7bf9f31d6cf652b5b12e5
Minyoung-Heo <heo8821@khu.ac.kr>
2023-10-20 16:44:52 +0900

    지역별 카페 보러가기 추가

--- ddabong_place/src/main/java/com/ezen/dda/cafe/CafeService.java
+++ ddabong_place/src/main/java/com/ezen/dda/cafe/CafeService.java
@@ -5,4 +5,6 @@ import java.util.ArrayList;
 public interface CafeService {
 	public ArrayList<CafeDTO> cafeOut(); // 전체 카페 보러가기
 	public ArrayList<CafeDTO> reviewStar(); // 별점 평균, 리뷰 수 출력
+	public ArrayList<CafeDTO> cafeRegion(String region_name); // 지역별 카페 보기
+	
 }
