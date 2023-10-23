f8aa6411fba579a928cf005529b5f5cf2dbe6f78
GaEun Pyo <pyo6659@naver.com>
2023-10-23 09:55:40 +0900

    마이페이지 비밀번호 틀릴시 돌아가는 창 수정

--- ddabong_place/src/main/java/com/ezen/dda/personalfunction/ReservationDTO.java
+++ ddabong_place/src/main/java/com/ezen/dda/personalfunction/ReservationDTO.java
@@ -6,113 +6,117 @@ int person_num;
 String reservation_num;
 String store_id, tel, address;
 String image;
-String RESERVATION_NAME;
-
-
-public String getImage() {
-	return image;
-}
-
-public void setImage(String image) {
-	this.image = image;
-}
-
-public String getRESERVATION_NAME() {
-	return RESERVATION_NAME;
-}
-
-public void setRESERVATION_NAME(String rESERVATION_NAME) {
-	RESERVATION_NAME = rESERVATION_NAME;
-}
+String reservation_name;
 
 public ReservationDTO() {
+   super();
 }
 
-public ReservationDTO(String store_img, String store_name, String reservation_date, String reservation_time,
-		int person_num, String reservation_num, String reservation_name, String store_tel, String store_addr) {
-	super();
-	this.store_img = store_img;
-	this.storename = storename;
-	this.reservation_date = reservation_date;
-	this.reservation_time = reservation_time;
-	this.person_num = person_num;
-	this.reservation_num = reservation_num;
-	this.reservation_name = reservation_name;
-	this.store_tel = store_tel;
-	this.store_addr = store_addr;
+public ReservationDTO(String store_img, String storename, String reservation_date, String reservation_time,
+      int person_num, String reservation_num, String store_id, String tel, String address, String image,
+      String reservation_name) {
+   super();
+   this.store_img = store_img;
+   this.storename = storename;
+   this.reservation_date = reservation_date;
+   this.reservation_time = reservation_time;
+   this.person_num = person_num;
+   this.reservation_num = reservation_num;
+   this.store_id = store_id;
+   this.tel = tel;
+   this.address = address;
+   this.image = image;
+   this.reservation_name = reservation_name;
 }
 
 public String getStore_img() {
-	return store_img;
+   return store_img;
 }
 
 public void setStore_img(String store_img) {
-	this.store_img = store_img;
+   this.store_img = store_img;
 }
 
-public String getStore_name() {
-	return store_name;
+public String getStorename() {
+   return storename;
 }
 
-public void setStore_name(String store_name) {
-	this.store_name = store_name;
+public void setStorename(String storename) {
+   this.storename = storename;
 }
 
 public String getReservation_date() {
-	return reservation_date;
+   return reservation_date;
 }
 
 public void setReservation_date(String reservation_date) {
-	this.reservation_date = reservation_date;
+   this.reservation_date = reservation_date;
 }
 
 public String getReservation_time() {
-	return reservation_time;
+   return reservation_time;
 }
 
 public void setReservation_time(String reservation_time) {
-	this.reservation_time = reservation_time;
+   this.reservation_time = reservation_time;
 }
 
 public int getPerson_num() {
-	return person_num;
+   return person_num;
 }
 
 public void setPerson_num(int person_num) {
-	this.person_num = person_num;
+   this.person_num = person_num;
 }
 
 public String getReservation_num() {
-	return reservation_num;
+   return reservation_num;
 }
 
 public void setReservation_num(String reservation_num) {
-	this.reservation_num = reservation_num;
+   this.reservation_num = reservation_num;
 }
 
-public String getReservation_name() {
-	return reservation_name;
+public String getStore_id() {
+   return store_id;
 }
 
-public void setReservation_name(String reservation_name) {
-	this.reservation_name = reservation_name;
+public void setStore_id(String store_id) {
+   this.store_id = store_id;
 }
 
-public String getStore_tel() {
-	return store_tel;
+public String getTel() {
+   return tel;
 }
 
-public void setStore_tel(String store_tel) {
-	this.store_tel = store_tel;
+public void setTel(String tel) {
+   this.tel = tel;
 }
 
-public String getStore_addr() {
-	return store_addr;
+public String getAddress() {
+   return address;
 }
 
-public void setStore_addr(String store_addr) {
-	this.store_addr = store_addr;
+public void setAddress(String address) {
+   this.address = address;
 }
+
+public String getImage() {
+   return image;
+}
+
+public void setImage(String image) {
+   this.image = image;
+}
+
+public String getreservation_name() {
+   return reservation_name;
+}
+
+public String setreservation_name(String reservation_name) {
+   return reservation_name = reservation_name;
+}
+
 
 
 
